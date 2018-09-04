7449e8697ea76ced31095c0f710ec296ec03d234
Pallavi Bhoja Naik <pallavibn9794@gmail.com>
2018-09-04 12:29:04 +0530

    Added Service file

---
+++ LocoFrameWork1/LocoFrameWork1/LocoFrameWork1.h
@@ -0,0 +1,19 @@
+//
+//  LocoFrameWork1.h
+//  LocoFrameWork1
+//
+//  Created by Pallavi Bhoja Naik on 04/09/18.
+//  Copyright © 2018 Pallavi Bhoja Naik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFrameWork1.
+FOUNDATION_EXPORT double LocoFrameWork1VersionNumber;
+
+//! Project version string for LocoFrameWork1.
+FOUNDATION_EXPORT const unsigned char LocoFrameWork1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFrameWork1/PublicHeader.h>
+
+
---
+++ LocoFrameWork1/LocoFrameWork1/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocoFrameWork1
+//
+//  Created by Pallavi Bhoja Naik on 04/09/18.
+//  Copyright © 2018 Pallavi Bhoja Naik. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static  func doSomething() -> String {
+        return "changed details"
+    }
+}
